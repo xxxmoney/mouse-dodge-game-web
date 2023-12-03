2071dd58270b9578e689977326376cf89e2b979c
xxxmoney <xxxmoney111@gmail.com>
2023-12-03 19:36:41 +0100

    Remove app.vue, add index.vue and tailwindcss.

---
+++ ClientApp/pages/index.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <div class="p-5 w-screen h-screen overflow-hidden">
+    <h1 class="text-center text-5xl">Welcome</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
