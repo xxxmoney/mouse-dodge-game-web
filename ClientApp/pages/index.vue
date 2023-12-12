1daa26d16cc8e1cff50a0f8743b732b035820ff9
xxxmoney <xxxmoney111@gmail.com>
2023-12-12 20:45:44 +0100

    Smol change.

--- ClientApp/pages/index.vue
+++ ClientApp/pages/index.vue
@@ -1,8 +1,4 @@
 
-<script setup lang="ts">
-
-</script>
-
 <template>
   <div class="px-5 py-10 flex flex-col gap-10 w-screen h-screen overflow-hidden">
     <h1 class="text-center text-5xl">Dodge!</h1>
